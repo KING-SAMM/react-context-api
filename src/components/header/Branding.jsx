a4bb4a3ad4d013af0d75375db4e229512d208a0f
kcsamm <kcsamm11@gmail.com>
2022-06-16 23:41:58 +0100

    Created Branding component

--- src/components/header/Branding.jsx
+++ src/components/header/Branding.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Branding = () => {
   return (
-    <div>Branding</div>
+    <div>
+      <a className="navbar-brand" href="#">
+        <img src="" alt="" width="30" height="24" className="d-inline-block align-text-top" />
+        LogoName
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,13 +7,7 @@ const Header = () => {
     <div>
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img src="" alt="" width="30" height="24" className="d-inline-block align-text-top" />
-            LogoName
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+          <Branding />
           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
               <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -24,7 +18,7 @@ const Header = () => {
           </div>
         </div>
       </nav>
-      {/* <Branding /> */}
+  
       {/* <Menu /> */}
     </div>
   )
